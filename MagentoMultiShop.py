31dd5ead396b51b2b3a0626dc6d50d9fc9c21f68
Troels Henningsen <elektroels@gmail.com>
2011-08-11 19:07:06 +0200

    dumdidej

--- MagentoMultiShop.py
+++ MagentoMultiShop.py
@@ -7,9 +7,23 @@ import mysql.connector
 
 conn = mysql.connector.Connect(host='127.0.0.1',user='connector',password='connector',database='connector')
 c = conn.cursor()
-#exempel #c.execute("""create table towns (#tid int primary key not NULL ,#name text,#postcode text)""")
 
 # sql oprettelse af shops og såvidere
+
+# opret website
+c.execute("""INSERT INTO core_website values (NULL , "websitetest", "websitetest", '0', '0', '0')""")
+# printe website_id også og bruge det i næste skridt
+
+# opret store                                               her website_id  navn  Root   Den her skal opdateres
+c.execute("""INSERT INTO core_store_group values (NULL , '0', 'storetest', '3', '0')""") 
+
+# opret storeview 
+#c.execute("""INSERT INTO core_store values ()""") 
+
+# print default_store_id og opdaterer sidsti rubrik
+#c.execute("""INSERT INTO core_store_group values (VALUES (NULL , '0', 'storetest', '3', '0')""") 
+
+conn.commit()
 
 # mkdirs
 # http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
--- sql_stuff_01.sql
+++ sql_stuff_01.sql
@@ -1,27 +1,42 @@
-
-/** Stores n websites
-Website Name  # http://1.bp.blogspot.com/-o_G5sn1bct8/TiGWupvb4kI/AAAAAAAAAJo/vpmbaHKG5zE/s1600/05%2Bwebsite%2Binfo.png
-Store Name    # http://3.bp.blogspot.com/-wig9rpuQMVI/TiGXh29gN9I/AAAAAAAAAJ4/RX1PDijpBpc/s1600/07%2Bsave%2Bstore.png
-Store View    # http://1.bp.blogspot.com/-YQ4FZFpR3Uk/TiGYm0jmw3I/AAAAAAAAAKI/mM1W4JbP1zw/s1600/07%2Bsave%2Bstore%2Bview.png
-**/
-
+/** Stores n websites **/
 /**
-	store_id 	code 	website_id 	group_id 	name 	sort_order 	is_active
-	 	0 	admin 	0 	0 	Admin 	0 	1
-	 	1 	default 	1 	1 	English 	0 	1
-		2 	german 	1 	1 	German 	0 	1
-	 	3 	french 	1 	1 	French 	0 	1
-	 	4 	troels 	2 	2 	Dansk 	0 	1
-**/
 
-/** core_store_group
+INSERT INTO `mc01`.`core_website` (`website_id` ,`code` ,`name` ,`sort_order` ,`default_group_id` ,`is_default`)
+VALUES (NULL , 'websitetest', 'websitetest', '0', '0', '0');
+
+ website_id 	code 	name 		sort_order 	default_group_id 	is_default
+		0 		admin 	Admin 			0 				0 				0
+		1 		base 		Main Website 	0 				1 				1
+		2 		troels 	troels 			0 				2			 	0
+
+
+
+
+
+INSERT INTO `mc01`.`core_store_group` (`group_id` ,`website_id` ,`name` ,`root_category_id` ,`default_store_id`)
+VALUES (NULL , '0', 'test', '0', '0');
 
 group_id 	website_id 	name 		root_category_id 	default_store_id
- 	0 		0 	Default 			0 			0
- 	1 		1 	Main Store 			3 			1
- 	2 		2 	troels 				3 			4		**/
+ 	0 		0 			Default 			0 			0
+ 	1 		1 			Main Store 			3 			1
+ 	2 		2 			troels 				3 			4
 
-/** core_config_data
+
+
+
+
+store_id 	code 	website_id 	group_id 	name 	sort_order 	is_active
+	 	0 		admin 		0 			0 		Admin	 	0 			1
+	 	1 		default 	1 			1 		English 	0 			1
+		2 		german 		1 			1 		German 		0 			1
+	 	3 		french 		1 			1 		French 		0	 		1
+	 	4 		troels 		2 			2 		Dansk 		0 			1
+
+
+
+
+
+insert into core_config_data
 
 	 54 	websites 	2 	web/unsecure/base_url       	http://127.0.0.1/mc01/shops/troels/
  	 55 	websites 	2 	web/secure/base_url 	        https://127.0.0.1/mc01/shops/troels/
@@ -39,4 +54,6 @@ group_id 	website_id 	name 		root_category_id 	default_store_id
  	101 	stores 	        4 	web/secure/base_media_url 	{{secure_base_url}}media/
  	102 	stores 	        4 	web/secure/base_js_url 	        {{secure_base_url}}js/
  	103 	stores 	        4 	web/default/cms_home_page 	home|5
- 	104 	stores 	        4 	general/store_information/name 	troels dansk			**/
+ 	104 	stores 	        4 	general/store_information/name 	troels dansk
+ 	
+**/
