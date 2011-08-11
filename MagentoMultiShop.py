3b670d79946379e3211028ce18bbda57e0abbadb
Troels Henningsen <elektroels@gmail.com>
2011-08-11 20:32:42 +0200

    dumdidej

--- MagentoMultiShop.py
+++ MagentoMultiShop.py
@@ -1,12 +1,30 @@
 ### Magento Multiple Shops Setup Python Script juli 2011.
+##
+#
+
+# imports
+import os, errno
+
+#functions
+def mkdir(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST:
+            pass
+        else: raise
+
+def write_file(filename, filecode):
+    newfile = open(filename, "w")
+    newfile.write(filecode)
+    newfile.close()
 
 # Database Connection Using mysql.connector
 # https://launchpad.net/myconnpy
 
-import mysql.connector
-
-conn = mysql.connector.Connect(host='127.0.0.1',user='connector',password='connector',database='connector')
-c = conn.cursor()
+#import mysql.connector
+#conn = mysql.connector.Connect(host='127.0.0.1',user='connector',password='connector',database='connector')
+#c = conn.cursor()
 
 # sql oprettelse af shops og såvidere
 
@@ -23,26 +41,20 @@ c.execute("""INSERT INTO core_store_group values (NULL , '0', 'storetest', '3', 
 # print default_store_id og opdaterer sidsti rubrik
 #c.execute("""INSERT INTO core_store_group values (VALUES (NULL , '0', 'storetest', '3', '0')""") 
 
-conn.commit()
+#conn.commit()
 
 # mkdirs
 # http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
-
-import os, errno
-
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST:
-            pass
-        else: raise
-
-#path = /opt/lampp/htdocs/mc01
-#path = path + shopnavn
-#mkdir_p(path)
+path = '/home/troels/Git/MagentoMultiShopPy/shops/'
+path = path + 'shopnavn'
+mkdir(path)
 
 # echo .htaccess	
+htaccesscode = "hallo du der"
+htaccess = path + '/.htaccess'
+write_file(htaccess, htaccesscode)
 
 # echo index.php
-# http://1.bp.blogspot.com/-zv9qS-tDuZc/TiiIu7UTpVI/AAAAAAAAALY/6N4YTrj9ebM/s1600/05%2Bindexphp%2Brun.png
+indexphpcode = "hallo du der"
+indexphp = path + '/index.php'
+write_file(indexphp, indexphpcode)
