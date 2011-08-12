47806e4a35f655e37d36cc32a262e498ad6dd86a
Troels Henningsen <elektroels@gmail.com>
2011-08-12 16:35:29 +0200

    opdateret lidt dbconn

--- dbconn.py
+++ dbconn.py
@@ -9,13 +9,18 @@ def mysql_connection():
         c = conn.cursor()
 
         # insert website info
-        c.execute("""INSERT INTO core_website values (NULL , "websitetest", "websitetest", '0', '0', '0')""")
+        websitename = 'websitetest'
+        insert_website_sql = "INSERT INTO core_website values (NULL , '" + websitename + "', '" + websitename + "', '0', '0', '0')"
+        
+        c.execute(insert_website_sql)
         conn.commit()
 
-        # fetch website_id 
-        c.execute ("""select website_id from core_website where name='websitetest'""")
+        # fetch website_id
 
-        # use website id
+        select_website_id_sql = "select website_id from core_website where name='" + websitename + "'"
+        c.execute (select_website_id_sql)
+
+        # use website id and clean it up #igang!!
         for row in c:
                 print(row)
                 
