c1099534dcfa51eabf76313e160a51bad1ff1dfa
Troels Henningsen <elektroels@gmail.com>
2011-08-13 11:18:40 +0200

    flere dbconn aendringer

--- dbconn.py
+++ dbconn.py
@@ -5,21 +5,39 @@ import mysql.connector
 import re
 
 # konfiguration
-websitename = "websitetest"
+
+#hoststr = "host='%s'"
+#usrstr = "user='%s'"
+#pwdstr = "password='%s'"
+#dbstr = "database='%s'"
+
+#host = "127.0.0.1"
+#user = "connector"
+#password = "connector"
+#database = "connector"
+
+websitename = "websitetest" # bliver også brugt som websitecode
 storename = "storetest"
+storeviewname = "storeviewtest"
 catalognr = "3"                 # 3 is standard Root Catalog
 
 # statements
 insert_website = "INSERT INTO core_website values (NULL , '%s', '%s', '0', '0', '0')"
 select_websiteid = "select website_id from core_website where name='%s'"
 insert_store = "INSERT INTO core_store_group values (NULL , '%s', '%s', '%s', '0')"
+#                                                 storeid  code webid  groupid name  sort  active
+insert_store_view = "INSERT INTO core_store values (NULL , '%s', '%s', '%s', '%s', '0', '1')"
+select_storeid ="select store_id from core_store where website_id='%s'"
+#update_store =""
 
 #from mystatements import insert_website, select_websiteid
 #from myconfig import websitename
 
+
 def mysql_connection():
         # connect to db
-        conn = mysql.connector.Connect(host='127.0.0.1',user='connector',password='connector',database='connector')
+        #print(hoststr % (host), usrstr % (user), pwdstr % (password), dbstr % (database))
+        conn = mysql.connector.Connect(host='127.0.0.1', user='connector', password='connector', database='connector') # hvordan gør jeg de her parametre dynamiske?!
         c = conn.cursor()
 
         c.execute(insert_website % (websitename, websitename))
@@ -40,9 +58,13 @@ def mysql_connection():
         conn.commit()
         
         # opret storeview 
-        #c.execute("""INSERT INTO core_store values ()""") 
-        # print default_store_id og opdaterer sidste rubrik
-        #c.execute("""INSERT INTO core_store_group values (VALUES (NULL , '0', 'storetest', '3', '0')""") 
+        c.execute(insert_store_view % (websitename, websiteid, websiteid, storeviewname))
+
+        # fetch store_view_id
+        c.execute(select_storeid % (websiteid))
+
+        # update store with store_view_id
+        #c.execute(update_store) 
 
         c.close()
 
