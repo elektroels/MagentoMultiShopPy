a78d83a1aa245415ce9113ff12e53c9525a8f571
Troels Henningsen <elektroels@gmail.com>
2011-08-13 16:12:26 +0200

    updates

--- MagentoMultiShop.py
+++ MagentoMultiShop.py
@@ -4,21 +4,23 @@
 
 # imports
 import os, errno
+from indexphp import indexphp
+from htaccess import htaccess
 
 magento_path = '/home/troels/Git/MagentoMultiShopPy/shops/'
 path = magento_path + 'shopnavn'
 
 def main():
-    # make directories		# http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
+        # make directories		# http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
 	mkdir(path)
+
+	#mysql_connection()
 	
 	# echo .htaccess
-	htaccesscode = "hallo du der" # erstattes af funktion
-	write_file('.htaccess', htaccesscode)
+	write_file('.htaccess', htaccess)
 	
 	# echo index.php
-	indexphpcode = "hallo du der" # erstattes af funktion
-	write_file('index.php', indexphpcode)
+	write_file('index.php', indexphp % (websitename))
 		
 
 #functions
@@ -35,33 +37,6 @@ def write_file(filename, filecode):
         newfile = open(path_file, "w")
         newfile.write(filecode)
         newfile.close()
-
-#def get_code_from_file
-
-#def get_options_from_file
-# Database Connection Using mysql.connector
-# https://launchpad.net/myconnpy
-
-#import mysql.connector
-#conn = mysql.connector.Connect(host='127.0.0.1',user='connector',password='connector',database='connector')
-#c = conn.cursor()
-
-# sql oprettelse af shops og såvidere
-
-# opret website
-#c.execute("""INSERT INTO core_website values (NULL , "websitetest", "websitetest", '0', '0', '0')""")
-# printe website_id også og bruge det i næste skridt
-
-# opret store                                               her website_id  navn  Root   Den her skal opdateres
-#c.execute("""INSERT INTO core_store_group values (NULL , '0', 'storetest', '3', '0')""") 
-
-# opret storeview 
-#c.execute("""INSERT INTO core_store values ()""") 
-
-# print default_store_id og opdaterer sidsti rubrik
-#c.execute("""INSERT INTO core_store_group values (VALUES (NULL , '0', 'storetest', '3', '0')""") 
-
-#conn.commit()
 
 
 if __name__ == "__main__":
